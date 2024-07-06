422aacbc186b3fea7de2664b673f5db76da61cb6
mkheck13 <mkheck13@gmail.com>
2024-07-06 08:56:01 -0700

    char from ASCII

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// 65 --> 'A'
+// 97 --> 'a'
+// 48 --> '0
+
+
+
+// for this one I'm going to the docs to see if there is a method that returns the specified string
+// String.fromCharCode()  is the method that returns the string
+function getChar(c){
+    return String.fromCharCode(c)
+}
